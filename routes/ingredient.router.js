5ffe19c7bfc8767ef7638cce06ba85e2c6dc7daa
Vicenç Servera <vserveraferrer@gmail.com>
2025-02-13 11:53:06 +0100

    US04 i US08 developed

--- routes/ingredient.router.js
+++ routes/ingredient.router.js
@@ -3,9 +3,10 @@ import db from '../database/db_schema.cjs';
 import validateCreateIngredientDTO from '../dto/validateCreateIngredientDTO.js';
 import authByToken from '../helpers/authByToken.js';
 import partialToCompleteIngredientMapper from '../mappers/partialToCompleteIngredient.mapper.js';
+import { where } from 'sequelize';
 
 const ingredientRouter = express.Router();
-const { Ingredient } = db;
+const { Ingredient, Type } = db;
 
 ingredientRouter.get("/get/:name", authByToken, async (req, res) => {
     const { name } = req.params;
@@ -16,6 +17,29 @@ ingredientRouter.get("/get/:name", authByToken, async (req, res) => {
 
     return res.status(200).send(JSON.stringify(ingredient))
 })
+
+ingredientRouter.get("/getAll", authByToken, async (req, res) => {
+    const ingredients = await Ingredient.findAll({where: { "userId": req.jwtData.payload.uuid }})
+
+    if(!ingredients) return res.status(200).send(JSON.stringify({message: "No hay ingredientes de este tipo"}));
+
+    const groupedIngredients = {}
+    
+    await Promise.all(
+        ingredients.map(async (ingredient) => {
+            const ingredientType = await Type.findOne({where: {"id": ingredient.ingredientType}}) 
+            const ingredientTypeName = ingredientType.name
+
+            if (!groupedIngredients[ingredientTypeName]) {
+                groupedIngredients[ingredientTypeName] = [];
+            }
+    
+            groupedIngredients[ingredientTypeName].push(ingredient);
+            })
+        );
+
+    return res.status(200).send(JSON.stringify({groupedIngredients}))
+});
 
 ingredientRouter.get("/getAll/:ingredientType", authByToken, async (req, res) => {
     const { ingredientType } = req.params;
@@ -37,6 +61,20 @@ ingredientRouter.post("/create", authByToken, validateCreateIngredientDTO, parti
     return res.status(200).send(JSON.stringify({
         message: "El ingrediente ha sido introducido"
     }));
+});
+
+ingredientRouter.post("/modify/:id", authByToken, partialToCompleteIngredientMapper, async (req, res) => {
+    const ingredientId = req.params.id;
+
+    if(!ingredientId) return res.status(404).send("No hay id de ingrediente en los parametros")
+
+    const ingredient = req.completeIngredient;
+
+    const [affectedCount, affectedRows] = await Ingredient.update(ingredient, {where: {"ingredientId": ingredientId}, returning: true})
+
+    if(affectedCount == 0) return res.status(200).send("No se modificó ningun elemento")
+
+    return res.status(200).send(affectedRows)
 });
 
 export default ingredientRouter;
