b557ae0015592d9e5727286e308d1f385f7de4ee
Vicenç Servera <vserveraferrer@gmail.com>
2025-03-04 12:34:25 +0100

    preparado para hacer funcion de creacion de matriz

--- helpers/buildMatrix.js
+++ helpers/buildMatrix.js
@@ -1,10 +1,34 @@
+// let matrix = {
+//     PAC: [],
+//     POD: [],
+//     MG: [],
+//     ST: [],
+//     LPD: [],
+//     percentCocoa: []
+// }
+import glpk from "glpk.js"
+
+const glpkInstance = await glpk();
+
 let matrix = {
-    PAC: [],
-    POD: [],
-    MG: [],
-    ST: [],
-    LPD: [],
-    percentCocoa: []
+    name: "",
+    objective: {
+        direction: glpkInstance.GLP_MIN,
+        name: "obj",
+        vars: []
+    },
+    subjectTo: [
+        { name: "PAC", vars: [], bnds: { type: glpkInstance.GLP_FX, ub: 280, lb: 280 } },
+        { name: "POD", vars: [], bnds: { type: glpkInstance.GLP_FX, ub: 280, lb: 280 } },
+        { name: "MG", vars: [], bnds: { type: glpkInstance.GLP_FX, ub: 280, lb: 280 } },
+        { name: "ST", vars: [], bnds: { type: glpkInstance.GLP_FX, ub: 280, lb: 280 } },
+        { name: "LPD", vars: [], bnds: { type: glpkInstance.GLP_FX, ub: 280, lb: 280 } },
+        { name: "percentCocoa", vars: [], bnds: { type: glpkInstance.GLP_FX, ub: 280, lb: 280 } },
+        { name: "suma1000", vars: [], bnds: { type: glpkInstance.GLP_FX, ub: 280, lb: 280 } }
+    ], 
+    bounds: [
+        
+    ],
 }
 
 const matrixKeys = Object.keys(matrix);
